83c927979f634cd81ae4ba54a053e38bce817f15
Jason Sendaula <sendaula@yahoo.com>
2025-10-22 23:18:35 -0400

    update template

--- create_or_update_youtube_playlist.py
+++ create_or_update_youtube_playlist.py
@@ -10,7 +10,7 @@ SCOPES = ["https://www.googleapis.com/auth/youtube"]
 
 def authenticate_youtube():
     """Authenticate and return an authorized YouTube API client."""
-    flow = InstalledAppFlow.from_client_secrets_file("client_secret_897398147357-i5hdois41qobv01lmfhgg14suh02s9fg.apps.googleusercontent.com.json", SCOPES)
+    flow = InstalledAppFlow.from_client_secrets_file("------------.json", SCOPES)
     creds = flow.run_local_server(port=0)
     youtube = build("youtube", "v3", credentials=creds)
     return youtube
